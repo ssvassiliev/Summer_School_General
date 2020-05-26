4fa6c5ca2cd1041e82a7a335fdfcee618c588406
Serguei Vassiliev <serguei.vassiliev@ace-net.ca>
2020-05-26 14:20:36 -0300

    add submission scripts

---
+++ code/submit-strong.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -A def-svassili
+#SBATCH --cpus-per-task=16
+#SBATCH --time=1:0
+#SBATCH --array=1-16%1
+
+rm output.csv
+./a.out 2000 2000 $SLURM_ARRAY_TASK_ID
+./a.out 2000 2000 $SLURM_ARRAY_TASK_ID
+./a.out 2000 2000 $SLURM_ARRAY_TASK_ID
---
+++ code/submit-weak.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -A def-svassili
+#SBATCH --cpus-per-task=16
+#SBATCH --time=10:00
+#SBATCH --array=1-16%1
+
+rm output.csv
+N=$SLURM_ARRAY_TASK_ID
+w=2000
+h=2000
+sw=$(printf '%.0f' `echo "scale=6;sqrt($N)*$w" | bc`)
+sh=$(printf '%.0f' `echo "scale=6;sqrt($N)*$h" | bc`)
+./a.out $sw $sh $N
+./a.out $sw $sh $N
+./a.out $sw $sh $N
+
+
