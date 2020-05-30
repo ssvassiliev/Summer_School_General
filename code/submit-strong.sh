435d0475d7065baca767230e5703dbb25012199f
Serguei Vassiliev <serguei.vassiliev@ace-net.ca>
2020-05-29 22:02:22 -0300

    fix submission

--- code/submit-strong.sh
+++ code/submit-strong.sh
@@ -4,7 +4,6 @@
 #SBATCH --time=1:0
 #SBATCH --array=1-16%1
 
-rm output.csv
 ./a.out 2000 2000 $SLURM_ARRAY_TASK_ID
 ./a.out 2000 2000 $SLURM_ARRAY_TASK_ID
 ./a.out 2000 2000 $SLURM_ARRAY_TASK_ID
--- code/submit-weak.sh
+++ code/submit-weak.sh
@@ -4,7 +4,6 @@
 #SBATCH --time=10:00
 #SBATCH --array=1-16%1
 
-rm output.csv
 N=$SLURM_ARRAY_TASK_ID
 w=2000
 h=2000
@@ -13,5 +12,3 @@ sh=$(printf '%.0f' `echo "scale=6;sqrt($N)*$h" | bc`)
 ./a.out $sw $sh $N
 ./a.out $sw $sh $N
 ./a.out $sw $sh $N
-
-
